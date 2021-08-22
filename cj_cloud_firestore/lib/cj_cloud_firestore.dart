a79f7fe79bb404208dd01f67023eb8455a908625
omazim <info.software@omazim.com>
2021-08-22 23:31:56 +0100

    Modified errors and removed unnecessary functions.

--- cj_cloud_firestore/lib/cj_cloud_firestore.dart
+++ cj_cloud_firestore/lib/cj_cloud_firestore.dart
@@ -19,7 +19,7 @@ class CjCloudFirestore {
   FirebaseFirestore _store = FirebaseFirestore.instance;
   WriteBatch get batch => _store.batch();  
 
-  String _collectionNameFromTableName (String tableName) {
+  String collectionNameFromTableName (String tableName) {
         
     String collName = "";
     final String pattern = r"^[a-z]+$"; 
@@ -58,7 +58,7 @@ class CjCloudFirestore {
           collName += "/" + v;
         } else {
           // If it is an even index, it's a collection name, append the realm suffix only if it is the root collection.
-          String loopCollName = _collectionNameFromTableName(v);
+          String loopCollName = collectionNameFromTableName(v);
 
           if (k > 0) {
             print("$v is a sub collection");
@@ -70,7 +70,7 @@ class CjCloudFirestore {
         }
       });
     } else {
-      collName = _collectionNameFromTableName(tableName) + _realm;
+      collName = collectionNameFromTableName(tableName) + _realm;
 
       if (docId.isNotEmpty) collName += "/" + docId;
     }
@@ -81,7 +81,7 @@ class CjCloudFirestore {
 
   CollectionReference collectionRef (String name) {
         
-    String collName = _collectionNameFromTableName(name) + _realm;
+    String collName = collectionNameFromTableName(name) + _realm;
     print("target collection => $collName");
     return _store.collection(collName);
   }  
@@ -103,7 +103,139 @@ class CjCloudFirestore {
     }
   }
 
-  Future<bool> batchWrite(List<MyBatchData> dataArray) async {
+  /// Read a collection by applying the query params (if any).
+  /// Return a list of the documents or document references depending on arguments.
+  Future<List<dynamic>?> readACollection(String tableName,
+      {MyFirestoreQueryParam? queryParam, bool getDocSnapshots: false}) async {
+    queryParam = queryParam ??= MyFirestoreQueryParam()..orderAsc = true;
+
+    final collRef = collectionRef(tableName);// Oma fixed this line 22 Aug 2021.
+    // Order by specified field or the first field in the first query criteria (if any).
+    // Otherwise no ordering.
+    // final bool descending = queryParam.orderAsc;
+
+    Query useQuery = collRef;
+    bool doNotRun = false;
+
+    queryParam.where = queryParam.where;
+
+    // If we have a query, we chain them in a loop.
+    queryParam.where.forEach((param) {
+      print(
+          "reading ${collRef.id} collection... where field => ${param.fieldName}, => ${param.op}, value => ${param.value}");
+      var value = param.value;
+
+      // if (value == null) {
+      //   doNotRun = true;
+      //   print("null value => ${param.value}");
+      // }
+
+      switch (param.op) {
+        case MyFirestoreFilterOp.isEqualTo:
+          print("where case is equal to");
+          useQuery = useQuery.where(param.fieldName, isEqualTo: value);
+          break;
+        case MyFirestoreFilterOp.isNotEqualTo:
+          useQuery = useQuery.where(param.fieldName, isNotEqualTo: value);
+          break;
+        case MyFirestoreFilterOp.isLessThan:
+          useQuery = useQuery.where(param.fieldName, isLessThan: value);
+          break;
+        case MyFirestoreFilterOp.isLessThanOrEqualTo:
+          useQuery =
+              useQuery.where(param.fieldName, isLessThanOrEqualTo: value);
+          break;
+        case MyFirestoreFilterOp.isGreaterThan:
+          useQuery = useQuery.where(param.fieldName, isGreaterThan: value);
+          break;
+        case MyFirestoreFilterOp.isGreaterThanOrEqualTo:
+          useQuery =
+              useQuery.where(param.fieldName, isGreaterThanOrEqualTo: value);
+          break;
+        case MyFirestoreFilterOp.arrayContains:
+          useQuery = useQuery.where(param.fieldName, arrayContains: value);
+
+          break;
+        case MyFirestoreFilterOp.arrayContainsAny:
+          useQuery = useQuery.where(param.fieldName, arrayContainsAny: value);
+
+          break;
+        case MyFirestoreFilterOp.whereIn:
+          useQuery = useQuery.where(param.fieldName, whereIn: value);
+
+          break;
+        case MyFirestoreFilterOp.whereNotIn:
+          useQuery = useQuery.where(param.fieldName, whereNotIn: value);
+
+          break;
+        case MyFirestoreFilterOp.isNull:
+          useQuery = useQuery.where(param.fieldName, isNull: value);
+          break;
+      }
+    });
+
+    try {
+      // if (queryParam.orderBy) != null) {
+
+      // useQuery = useQuery.orderBy(queryParam.orderBy, descending: descending);
+      // } else if (queryParam.where.length > 0) {
+
+      // useQuery = useQuery.orderBy(queryParam.where[0].fieldName, descending: descending);
+      // }
+
+      print("do not run $doNotRun");
+
+      // if (doNotRun) {
+      //   print("do not run query");
+      //   return [];
+      // }
+      QuerySnapshot snapshot = await useQuery.get();
+
+      print("read ${snapshot.docs.length} docs from collection ${collRef.id}");
+
+      if (snapshot.docs.length > 0) {
+        final list = getDocSnapshots
+            ? snapshot.docs
+            : snapshot.docs.map((doc) => doc.data()).toList();
+
+        return list;
+      } else {
+        return [];
+      }
+    } catch (err) {
+      print("error reading collection $tableName \n $err");
+
+      return null;
+    }
+  }
+
+  /// Return a single document snapshot for the passed collection and doc id.
+  Future<dynamic> readADocumentById(String tableName, String docId, {bool getDocSnapshot: false}) async {
+    // String collName = collectionNameFromTableName(tableName);
+    DocumentSnapshot? snapshot = await collectionRef(tableName).doc(docId).get();
+
+    if (snapshot.exists) {
+      return getDocSnapshot ? snapshot : snapshot.data();
+    } else {
+      return null;
+    }
+  }
+
+  /// Return a single document for the passed collection and query.
+  Future<dynamic> readADocumentByQuery(String tableName, MyFirestoreQueryParam queryParam, {bool getDocSnapshot: false}) async {
+    queryParam = queryParam;
+
+    final dataOrSnapshotsOrNull = await readACollection(tableName,
+        queryParam: queryParam, getDocSnapshots: getDocSnapshot);
+
+    if (dataOrSnapshotsOrNull == null) return null;
+
+    if (dataOrSnapshotsOrNull.length == 0) return null;
+
+    return dataOrSnapshotsOrNull[0];
+  }
+
+  Future<bool> batchWrite (List<MyBatchData> dataArray) async {
     final WriteBatch b = batch;
     print("write ${dataArray.length} items in batch");
     dataArray.forEach((data) {
